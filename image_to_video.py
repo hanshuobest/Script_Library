bf8817d4b95a0cacd213a454d700f7fa00a2b6e0
hanshuobest <hscoder@163.com>
2020-09-24 23:02:40 +0800

    图片合成视频

--- image_to_video.py
+++ image_to_video.py
@@ -1,7 +1,4 @@
 # -*- coding:utf-8 -*-
-import sys
-reload(sys)
-sys.setdefaultencoding('utf-8')
 
 # 将连续的图片另存为视频
 
@@ -18,30 +15,19 @@ fps = 25
 
 img_lsts = glob.glob(img_dir + "/*.jpg")
 # print(img_lsts)
-
-
-name_lst = []
-for i in img_lsts:
-	split_num = int(os.path.basename(i)[:-4].split('_')[1])
-	# print(split_num)
-	name_lst.append(split_num)
-	
-name_lst.sort()
-print(name_lst)
+img_lsts.sort(key = lambda item : int(os.path.basename(item)[:-4]))
 
 img_0 = cv2.imread(img_lsts[0])
 h , w , _ = img_0.shape
 img_size = (w , h)
 print(img_size)
 
-
-
 fourcc = cv2.VideoWriter_fourcc('M','J','P','G')
 videoWriter = cv2.VideoWriter(video_dir, fourcc, fps, img_size)
 
-for i in tqdm(range(len(name_lst))):
+for i in tqdm(range(len(img_lsts))):
 	
-	img = cv2.imread(os.path.join(img_dir ,str(file_id) + "_" + str(name_lst[i]) + ".jpg"))
+	img = cv2.imread(img_lsts[i])
 	if type(img) == type(None):
 		print('read image failture!')
 		break
