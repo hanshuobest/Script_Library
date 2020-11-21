bfe4e195bd50e13cba6552b6f9983276268eeb05
hanshuobest <hscoder@163.com>
2020-11-21 18:10:21 +0800

    2020-11-21

--- create_train_test.py
+++ create_train_test.py
@@ -1,4 +1,4 @@
-#coding:utf-8
+# coding:utf-8
 # 将数据分成训练集和测试集单独保存
 import os
 import random
@@ -6,60 +6,84 @@ import glob
 import numpy as np
 import shutil
 
-if __name__ == '__main__':
-        xml_lsts = glob.glob(os.getcwd() + "/*.xml")
-        total_num = len(xml_lsts)
-        train_percent = 0.8
-        test_percent = 0.2
 
-        num_lst = range(total_num)
-        np.random.shuffle(num_lst)
+def isExist(dir_path):
+    if os.path.exists(dir_path):
+        shutil.rmtree(dir_path)
+        os.mkdir(dir_path)
+    else:
+        os.mkdir(dir_path)
 
-        train_num = int(train_percent * total_num)
-        test_num = int(test_percent * total_num)
-        
-        train = random.sample(num_lst , train_num)
-        
-        train_dir = "Train"
-        test_dir   = "Test"
 
-        if os.path.exists(train_dir):
-	        shutil.rmtree(train_dir)
-        else:
-	        os.mkdir(train_dir)
-	        pass
-        
-        if os.path.exists(test_dir):
-	        shutil.rmtree(test_dir)
-        else:
-	        os.mkdir(test_dir)
-	        pass
-        
-        
-        if os.path.isdir(train_dir):
-            pass
-        else:
-            os.mkdir(train_dir)
-	    
-        if os.path.isdir(test_dir):
-             pass
-        else:
-            os.mkdir(test_dir)
-        
-	        
-        
-        for i in num_lst:
+if __name__ == "__main__":
+    xml_lsts = glob.glob(os.getcwd() + "/*.xml")
+    total_num = len(xml_lsts)
+
+    trainval_percent = 0.9
+    train_percent = 0.9
+    test_percent = 0.1
+
+    num_lst = list(range(total_num))
+    np.random.shuffle(num_lst)
+
+    train_val_num = int(trainval_percent * total_num)
+
+    train_num = int(train_percent * total_num)
+    test_num = int(test_percent * total_num)
+
+    trainval = random.sample(num_lst, train_val_num)
+    train = random.sample(trainval, int(train_percent * train_val_num))
+
+    train_dir = "Train"
+    test_dir = "Test"
+    val_dir = "Val"
+
+    isExist(train_dir)
+    isExist(test_dir)
+    isExist(val_dir)
+
+    for i in num_lst:
+        if i in trainval:
             if i in train:
-                shutil.copyfile(xml_lsts[i], os.getcwd() + "/" + train_dir + "/" + os.path.basename(xml_lsts[i]))
-                shutil.copyfile(xml_lsts[i][:-3] + "jpg", os.getcwd() + "/" + train_dir + "/" + os.path.basename(xml_lsts[i])[:-3] + "jpg")
+                shutil.copyfile(
+                    xml_lsts[i],
+                    os.getcwd() + "/" + train_dir + "/" + os.path.basename(xml_lsts[i]),
+                )
+                shutil.copyfile(
+                    xml_lsts[i][:-3] + "jpg",
+                    os.getcwd()
+                    + "/"
+                    + train_dir
+                    + "/"
+                    + os.path.basename(xml_lsts[i])[:-3]
+                    + "jpg",
+                )
             else:
-	            shutil.copyfile(xml_lsts[i], os.getcwd() + "/" + test_dir + "/" + os.path.basename(xml_lsts[i]))
-	            shutil.copyfile(xml_lsts[i][:-3] + "jpg",
-	                            os.getcwd() + "/" + test_dir + "/" + os.path.basename(xml_lsts[i])[:-3] + "jpg")
-	            
-	    
-                
-	        
-             
-			
-						
+                shutil.copyfile(
+                    xml_lsts[i],
+                    os.getcwd() + "/" + val_dir + "/" + os.path.basename(xml_lsts[i]),
+                )
+                shutil.copyfile(
+                    xml_lsts[i][:-3] + "jpg",
+                    os.getcwd()
+                    + "/"
+                    + val_dir
+                    + "/"
+                    + os.path.basename(xml_lsts[i])[:-3]
+                    + "jpg",
+                )
+
+        else:
+            shutil.copyfile(
+                xml_lsts[i],
+                os.getcwd() + "/" + test_dir + "/" + os.path.basename(xml_lsts[i]),
+            )
+            shutil.copyfile(
+                xml_lsts[i][:-3] + "jpg",
+                os.getcwd()
+                + "/"
+                + test_dir
+                + "/"
+                + os.path.basename(xml_lsts[i])[:-3]
+                + "jpg",
+            )
--- select_skus.py
+++ select_skus.py
@@ -1,9 +1,6 @@
-#coding:utf-8
+# coding:utf-8
 
 import sys
-reload(sys)
-sys.setdefaultencoding('utf8')
-
 import xml.etree.cElementTree as ET
 import os
 import glob
@@ -12,59 +9,64 @@ import argparse
 from tqdm import tqdm
 import time
 
-def filter_xml(xml_path , class_id , newdir):
-	et = ET.parse(xml_path)
-	element = et.getroot()
-	element_objs = element.findall('object')
-	
-	suffix = None
-	if os.path.exists(xml_path[:-3] + "jpg"):
-		suffix = "jpg"
-	elif os.path.exists(xml_path[:-3] + "png"):
-		suffix = "png"
-	
-	jpg_dir = os.path.dirname(xml_path)
-	
-	for element_obj in element_objs:
-		class_name = element_obj.find('name').text
-		if class_name == class_id:
-			shutil.copyfile(xml_path , newdir + '/' + os.path.basename(xml_path))
-			shutil.copyfile(xml_path[:-3] + suffix , newdir + '/' + os.path.basename(xml_path)[:-3] + suffix)
 
-if __name__ == '__main__':
-        classes = ['wire' , 'hair' , 'pen']	
-	for i in classes:
-		new_dir = str(i) + "-sku"
-		if os.path.exists(new_dir):
-                	shutil.rmtree(new_dir)
-      	  	else:
-                	os.mkdir(new_dir)
-        	if os.path.isdir(new_dir):
-                	pass
-        	else:
-                	os.mkdir(new_dir)
-	xml_dir = os.getcwd()
-	lsts = glob.glob(xml_dir + "/*.xml")
-	
-	start_time = time.time()
-	for i in lsts:
-		et = ET.parse(i)
-        	element = et.getroot()
-        	element_objs = element.findall('object')
+def filter_xml(xml_path, class_id, newdir):
+    et = ET.parse(xml_path)
+    element = et.getroot()
+    element_objs = element.findall("object")
 
-        	suffix = None
-        	if os.path.exists(i[:-3] + "jpg"):
-                	suffix = "jpg"
-        	elif os.path.exists(i[:-3] + "png"):
-                	suffix = "png"
-		for element_obj in element_objs:
-			class_name = element_obj.find('name').text.strip()
-			if class_name in classes:
-				new_dir = os.path.join(os.getcwd() , str(class_name) + "-sku")
-				shutil.copyfile(i , new_dir + "/" + os.path.basename(i))
-				shutil.copyfile(i[:-3] + suffix , new_dir + "/" + os.path.basename(i)[:-3] + suffix)
-			else:
-				break
-	cost_time = time.time() - start_time
-	print('cost time:' , cost_time)
+    suffix = None
+    if os.path.exists(xml_path[:-3] + "jpg"):
+        suffix = "jpg"
+    elif os.path.exists(xml_path[:-3] + "png"):
+        suffix = "png"
 
+    jpg_dir = os.path.dirname(xml_path)
+
+    for element_obj in element_objs:
+        class_name = element_obj.find("name").text
+        if class_name == class_id:
+            shutil.copyfile(xml_path, newdir + "/" + os.path.basename(xml_path))
+            shutil.copyfile(
+                xml_path[:-3] + suffix,
+                newdir + "/" + os.path.basename(xml_path)[:-3] + suffix,
+            )
+
+
+if __name__ == "__main__":
+    classes = ["wire", "hair", "pen"]
+    for i in classes:
+        new_dir = str(i) + "-sku"
+        if os.path.exists(new_dir):
+            shutil.rmtree(new_dir)
+        else:
+            os.mkdir(new_dir)
+        if os.path.isdir(new_dir):
+            pass
+        else:
+            os.mkdir(new_dir)
+    xml_dir = os.getcwd()
+    lsts = glob.glob(xml_dir + "/*.xml")
+
+    start_time = time.time()
+
+    for i in lsts:
+        et = ET.parse(i)
+        element = et.getroot()
+        element_objs = element.findall("object")
+
+        suffix = None
+        if os.path.exists(i[:-3] + "jpg"):
+            suffix = "jpg"
+        elif os.path.exists(i[:-3] + "png"):
+            suffix = "png"
+        for element_obj in element_objs:
+            class_name = element_obj.find("name").text.strip()
+            if class_name in classes:
+                new_dir = os.path.join(os.getcwd(), str(class_name) + "-sku")
+                shutil.copyfile(i, new_dir + "/" + os.path.basename(i))
+                shutil.copyfile(
+                    i[:-3] + suffix, new_dir + "/" + os.path.basename(i)[:-3] + suffix
+                )
+            else:
+                break
